cef59e6b55e64d0f43020f2c7f871472dcb0c5b0
Layoute <qingfengroot@163.com>
2022-05-24 15:52:14 +0800

    support query for location id

--- src/LaravelWeather.php
+++ src/LaravelWeather.php
@@ -2,6 +2,11 @@
 
 namespace Layoute\LaravelWeather;
 
+use GuzzleHttp\Client;
+use GuzzleHttp\Exception\GuzzleException;
+use Illuminate\Support\Facades\Log;
+use Layoute\LaravelWeather\Exception\APIKeyNotFoundException;
+
 class LaravelWeather
 {
     protected $config;
@@ -13,14 +18,74 @@ class LaravelWeather
     }
 
     /**
+     * @throws APIKeyNotFoundException
+     */
+    private function getEnv(): array
+    {
+        if (isset($this->config)) {
+            if ($this->config['dev_mode']) {
+                return $this->config['dev'];
+            } else {
+                return $this->config['prod'];
+            }
+        } else {
+            throw new APIKeyNotFoundException();
+        }
+    }
+
+    /**
+     * @throws APIKeyNotFoundException
+     */
+    private function getAppKey(): string
+    {
+        if (isset($this->config)) {
+            return $this->config['qweather']['key'];
+        } else {
+            throw new APIKeyNotFoundException();
+        }
+    }
+
+    /**
      * Get real-time weather info for specified city
-     * @param string $location location id for city
+     * @param string $locationId location id for city
      * @param string $lang language setting for response
      * @return string
      */
-    public function queryWeatherForCity(string $location, string $lang = "zh"): string
+    public function queryWeatherForCity(string $locationId, string $lang = "zh"): string
     {
 
         return "";
+    }
+
+    /**
+     * @throws APIKeyNotFoundException
+     */
+    public function getLocationIdByName(string $city_name, string $adm = null, string $range = "world", string $lang = "zh"): string
+    {
+        $env = $this->getEnv();
+        if (isset($this->config)) {
+            $endpoint = $env['geo_base_url'] . '/city/lookup?location=' . $city_name . '&key=' . $this->getAppKey() . '&adm=' . $adm . '&range=' . $range . '&lang=' . $lang;
+            return "";
+        } else {
+            throw new APIKeyNotFoundException();
+        }
+    }
+
+    /**
+     * @throws APIKeyNotFoundException
+     * @throws GuzzleException
+     */
+    public function getLocationIdByCoordinate(float $lat, float $lng, string $adm = null, string $range = "world", string $lang = "zh"): string
+    {
+        $env = $this->getEnv();
+        if (isset($this->config)) {
+            $endpoint = $env['geo_base_url'] . '/city/lookup?location=' . $lat . '.' . $lng . '&key=' . $this->getAppKey() . '&adm=' . $adm . '&range=' . $range . '&lang=' . $lang;
+            $http = new Client;
+            $response = $http->get($endpoint);
+            Log::info($response->getBody());
+            return "";
+        } else {
+            throw new APIKeyNotFoundException();
+        }
     }
 }
---
+++ src/exception/InvalidConfigurationException.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Layoute\LaravelWeather\Exception;
+
+use Exception;
+use Illuminate\Http\JsonResponse;
+use Throwable;
+
+class APIKeyNotFoundException extends Exception
+{
+    public function __construct($message = "API Key Not Found", $code = 500, Throwable $previous = null)
+    {
+        parent::__construct($message, $code, $previous);
+    }
+
+    public function render(): JsonResponse
+    {
+        return response()->json([
+            'code' => $this->code,
+            'message' => $this->message,
+        ]);
+    }
+}
